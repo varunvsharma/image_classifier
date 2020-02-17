7753604a44fa15b5b63f5c5cf664eac6d9483af1
Varun Sharma <varunvsharma@gmail.com>
2020-02-17 22:11:15 +1100

    feat: Add image processing function to data processing module

--- process_data.py
+++ process_data.py
@@ -69,3 +69,51 @@ def load_data(data_dir):
     test_dataset = datasets.ImageFolder(test_dir, transform=test_transform)
 
     return train_dataset, valid_dataset, test_dataset
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model and returns a Numpy array.
+        
+        Keyword arguments:
+        image -- PIL image
+    '''
+    # Get image attributes and compute aspect ratio
+    width, height = image.size
+    aspect_ratio = (width / height)
+    
+    # Resize images
+    # Make the shortest size 256 and maintain the aspect ratio
+    if width < height:
+        new_width = 256
+        new_height = int(np.round(new_width/aspect_ratio))
+        image = image.resize((new_width, new_height))
+    elif width > height:
+        new_height = 256
+        new_width = int(np.round(new_height*aspect_ratio))
+        image = image.resize((new_width, new_height))
+        
+    # Centre Crop image
+    centre_crop_size = 224
+    w_margin = (new_width - centre_crop_size)/2
+    h_margin = (new_height - centre_crop_size)/2
+    left = w_margin
+    right = w_margin + centre_crop_size
+    upper = h_margin
+    lower = h_margin + centre_crop_size
+    image = image.crop(box=(left, upper, right, lower))
+    
+    
+    # Convert image into a numpy array
+    np_image = np.array(image)
+    
+    # Transpose array and make colour channels the first dimension
+    np_image = np_image.transpose(2, 0, 1)
+    
+    # Scale the values in each colour channel to a value between 0 and 1
+    np_image = np_image / 255
+    
+    # Normalize the image
+    means = np.array([0.485, 0.456, 0.406])[:, None, None]
+    stds = np.array([0.229, 0.224, 0.225])[:, None, None]
+    np_image = (np_image - means) / stds
+    
+    return np_image    
